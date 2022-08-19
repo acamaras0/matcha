11d3757accaa11fc4fcc286f74d7a04fb539e5ce
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-08-19 11:33:54 +0300

    index.js cleaned

--- back/controllers/Images.js
+++ back/controllers/Images.js
@@ -1,8 +1,25 @@
 import Users from "../models/UserModel.js";
 import UserImages from "../models/ImageModel.js";
-import multer from "multer";
 
-
-
-
-
+export const UploadPic = async (req, res) => {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded");
+  }
+  const user = await Users.findOne({
+    where: {
+      online: 1,
+    },
+  });
+  const userId = user.dataValues.id;
+  try {
+    await UserImages.create({
+      user_id: userId,
+      pic_name: req.file.filename,
+      pic_path: "./uploads/" + req.file.filename,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+  console.log("File uploaded successfully");
+  res.sendStatus(200);
+};
--- back/routes/index.js
+++ back/routes/index.js
@@ -7,12 +7,10 @@ import {
   Logout,
   getLoggedIn,
 } from "../controllers/Users.js";
+import { UploadPic } from "../controllers/Images.js";
 import { upload } from "../middleware/Upload.js";
 import { verifyToken } from "../middleware/VerifyToken.js";
 import { refreshToken } from "../controllers/RefreshToken.js";
-//import Resize from "../middleware/Resize.js";
-import UserImages from "../models/ImageModel.js";
-import Users from "../models/UserModel.js";
 
 const router = express.Router();
 
@@ -20,34 +18,12 @@ router.get("/users", getLoggedIn);
 router.get("/users/info", getUsers);
 router.get("/users/verify", verifyToken);
 router.get("/token", refreshToken);
+
+
 router.post("/users", Register);
 router.post("/login", Login);
 router.post("/fill", ProfileFill);
-
-router.post("/upload", upload.single("file"), async (req, res) => {
-  if (!req.file) {
-    return res.status(400).send("No file uploaded");
-  } else {
-    console.log(req.file.filename);
-    const imgsrc = "./uploads/" + req.file.filename;
-    const user = await Users.findOne({
-      where: {
-        online: 1,
-      },
-    });
-    const userId = user.dataValues.id;
-    try {
-      await UserImages.create({
-        user_id: userId,
-        pic_name: req.file.filename,
-        pic_path: imgsrc,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  }
-  console.log("File uploaded successfully");
-});
+router.post("/upload", upload.single("file"), UploadPic);
 
 router.delete("/logout", Logout);
 

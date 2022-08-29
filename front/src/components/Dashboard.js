3c400908d53821454e55ccf163cc1650e127cc3c
Anamaria Camarasan <anamaria.finn0@gmail.com>
2022-08-29 14:20:57 +0300

    conditions of showing users added, like/dislike in progress

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import jwt_decode from "jwt-decode";
 import { useHistory } from "react-router-dom";
 import StarRating from "../models/StarRating";
+import img from "../assets/yellow-heart.png";
+import img1 from "../assets/broken-heart.png";
 
 const Dashboard = () => {
   const [loggedIn, setLoggedin] = useState("");
@@ -94,34 +96,40 @@ const Dashboard = () => {
       <div className="">
         {users &&
           users.map((user) => {
-            return (
-              <div key={user.id} className="card mb-3">
-                <div className="row no-gutters">
-                  <div className="col-md-4">
-                    <img
-                      onClick={() => handleUserSelect(user.id)}
-                      className="card-img"
-                      src={user.profile_pic}
-                      alt="Card cap"
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        {user.username}, {user.birthdate}
-                      </h5>
-                      <StarRating rating={5} /> <br />
-                      <label>Gender</label>
-                      <p className="card-text">{user.gender}</p>
-                      <label>Location</label>
-                      <p className="card-text">{user.city}</p>
-                      <label>Bio</label>
-                      <p className="card-text">{user.bio}</p>
+            if (user.profile_pic) {
+              return (
+                <div key={user.id} className="card mb-3">
+                  <div className="row no-gutters">
+                    <div className="col-md-4">
+                      <img
+                        onClick={() => handleUserSelect(user.id)}
+                        className="card-img"
+                        src={user.profile_pic}
+                        alt="Card cap"
+                      />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {user.username}, {user.birthdate}
+                        </h5>
+                        <StarRating rating={5} /> <br />
+                        <label>Gender</label>
+                        <p className="card-text">{user.gender}</p>
+                        <label>Location</label>
+                        <p className="card-text">{user.city}</p>
+                        <label>Bio</label>
+                        <p className="card-text">{user.bio}</p>
+                        <div className="like-container">
+                          <img className="like" src={img} alt="Card cap" />
+                          <img className="dislike" src={img1} alt="Card cap" />
+                        </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
+              );
+            }
           })}
       </div>
     </div>

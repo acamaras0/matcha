c936870ef08e694465b9662cfb28ddc86acbd451
Anamaria Camarasan <acamaras@student.hive.fi>
2022-10-03 18:19:54 +0300

    profile recommendations done

--- front/src/components/Dashboard.js
+++ front/src/components/Dashboard.js
@@ -6,15 +6,17 @@ import useGetDistance from "../utils/useGetDistance";
 import { format } from "timeago.js";
 import { UserContext } from "../context/UserContext";
 import { getCookie } from "react-use-cookie";
+import Recommended from "../models/Recommended";
 
 const Dashboard = ({ socket }) => {
   const { user, setUser } = useContext(UserContext);
   const [message, setMessage] = useState([]);
   const [sortState, setSortState] = useState("none");
+  const [show, setShow] = useState(false);
   const history = useHistory();
   const distance = useGetDistance();
   const xsrfToken = getCookie("refreshToken");
-
+  let filtered = [];
   useEffect(() => {
     if (xsrfToken !== "") {
       const getLoggedIn = async () => {
@@ -54,32 +56,38 @@ const Dashboard = ({ socket }) => {
     return <Redirect to="/" />;
   }
 
-
-  // let filtered = [];
-
-  // distance.forEach((u) => {
-  //   let result = u.interests.split(",");
-  //   for (let index = 0; index < result.length; index++) {
-  //     result[index] = result[index].replace(/^\s+|\s+$/gm, "");
-  //   }
-  //   const multipleExist = user.interests.every((value) => {
-  //     return result.includes(value);
-  //   });
-  //   if (multipleExist) {
-  //     filtered.push(u);
-  //   }
-  // });
-
   const sortMethods = {
     none: { method: (a, b) => null },
     location: { method: (a, b) => a.distance - b.distance },
     fame: { method: (a, b) => b.fame - a.fame },
     age: { method: (a, b) => a.birthdate - b.birthdate },
-    tags:{method:(a, b) => a.interests - b.interests}
   };
 
   if (distance) {
     distance.sort(sortMethods[sortState].method);
+    distance.forEach((u) => {
+      let result = u.interests.split(",");
+      for (let index = 0; index < result.length; index++) {
+        result[index] = result[index].replace(/^\s+|\s+$/gm, "");
+      }
+      let loggedInterests = [];
+      if (user.interests) {
+        loggedInterests = user.interests.split(",");
+        for (let index = 0; index < loggedInterests.length; index++) {
+          loggedInterests[index] = loggedInterests[index].replace(
+            /^\s+|\s+$/gm,
+            ""
+          );
+        }
+        const multipleExist = loggedInterests.some((value) => {
+          return result.includes(value);
+        });
+        if (multipleExist) {
+          filtered.push(u);
+        }
+      }
+      return filtered;
+    });
   }
 
   if (distance.length === 0)
@@ -104,65 +112,72 @@ const Dashboard = ({ socket }) => {
           <option value="location">Distance</option>
           <option value="age">Age</option>
           <option value="fame">Popularity</option>
-          <option value="tags">Common tags</option>
         </select>
       </div>
+      <div className="recommended-btn">
+        <button className="btn btn-warning" onClick={() => setShow(!show)}>
+          {" "}
+          You have the most in common with ▼
+        </button>
+      </div>
+      {show ? (
+        <Recommended filtered={filtered} socket={socket} user={user} />
+      ) : null}
       <div className="dashboard">
         {distance &&
           distance.map((user) => {
-            if (user.profile_pic.length > 0 ) {
-            return (
-              <div
-                key={user.id}
-                className="card mb-4"
-                style={{ width: "15rem" }}
-              >
-                <div className="row no-gutters">
-                  <div>
-                    <img
-                      onClick={() => handleUserSelect(user.id)}
-                      className="card-img img-fluid"
-                      src={user.profile_pic}
-                      alt="Card cap"
-                    />
-                  </div>
-                  <div className="col-md-8">
-                    <div className="card-body">
-                      <h5 className="card-title">
-                        {user.username}, {user.birthdate}{" "}
-                        {user.online === 1 ? (
-                          <p className="online"></p>
-                        ) : (
-                          <p className="offline"></p>
-                        )}
-                      </h5>
-                      <h6 className="card-title">
-                        {user.city}, {user.country}{" "}
-                      </h6>
-                      <StarRating rating={user.fame} /> <br />
-                      <p className="card-text">
-                        About {Math.round(user.distance / 1000)} km away
-                      </p>
-                      <div>
-                        <a
-                          onClick={() => handleReport(user.id)}
-                          className="report"
-                          href="http://localhost:3000/dashboard"
-                        >
-                          Report fake account
-                        </a>
-                        <p className="report">
-                          Last seen {format(user.updated_at)}
+            if (user.profile_pic.length > 0) {
+              return (
+                <div
+                  key={user.id}
+                  className="card mb-4"
+                  style={{ width: "30rem" }}
+                >
+                  <div className="row no-gutters">
+                    <div>
+                      <img
+                        onClick={() => handleUserSelect(user.id)}
+                        className="card-img img-fluid"
+                        src={user.profile_pic}
+                        alt="Card cap"
+                      />
+                    </div>
+                    <div className="col-md-8">
+                      <div className="card-body">
+                        <h5 className="card-title">
+                          {user.username}, {user.birthdate}{" "}
+                          {user.online === 1 ? (
+                            <p className="online"></p>
+                          ) : (
+                            <p className="offline"></p>
+                          )}
+                        </h5>
+                        <h6 className="card-title">
+                          {user.city}, {user.country}{" "}
+                        </h6>
+                        <StarRating rating={user.fame/2} /> <br />
+                        <p className="card-text">
+                          About {Math.round(user.distance / 1000)} km away
                         </p>
+                        <div>
+                          <a
+                            onClick={() => handleReport(user.id)}
+                            className="report"
+                            href="http://localhost:3000/dashboard"
+                          >
+                            Report fake account
+                          </a>
+                          <p className="report">
+                            Last seen {format(user.updated_at)}
+                          </p>
+                        </div>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            );
-            }
-            else {
-              return(<p> Upload your picture to get started!</p>)
+              );
+            } else {
+              return <p> Upload your picture to get started!</p>;
             }
           })}
       </div>
---
+++ front/src/models/Recommended.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import StarRating from "./StarRating";
+
+const Recommended = ({ filtered, socket, user }) => {
+    const history = useHistory();
+  const handleUserSelect = async (id) => {
+    if (socket) {
+      socket.emit("sendNotification", {
+        senderName: user.username,
+        senderId: user.id,
+        receiverName: id,
+        type: "profile view",
+      });
+    }
+    history.push(`/users/${id}`);
+  };
+
+  return (
+    <div className="recommended">
+      {filtered &&
+        filtered.map((user) => {
+          if (user.profile_pic.length > 0) {
+            return (
+              <div
+                key={user.id}
+                className="card mb-4"
+                style={{ width: "12rem" }}
+              >
+                <div className="row no-gutters">
+                  <div>
+                    <img
+                      onClick={() => handleUserSelect(user.id)}
+                      className="card-img img-fluid"
+                      src={user.profile_pic}
+                      alt="Card cap"
+                    />
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body">
+                      <h5 className="card-title">
+                        {user.username}, {user.birthdate}{" "}
+                        {user.online === 1 ? (
+                          <p className="online"></p>
+                        ) : (
+                          <p className="offline"></p>
+                        )}
+                      </h5>
+                      <h6 className="card-title">
+                        {user.city}, {user.country}{" "}
+                      </h6>
+                      <StarRating rating={user.fame} /> <br />
+                      <p className="card-text">
+                        About {Math.round(user.distance / 1000)} km away
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            );
+          } else {
+            return <p> Upload your picture to get started!</p>;
+          }
+        })}
+    </div>
+  );
+};
+
+export default Recommended;

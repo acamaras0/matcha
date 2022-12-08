ffe0939aaf310113911b474207933c780659131f
Anamaria Camarasan <acamaras@student.hive.fi>
2022-12-08 17:15:17 +0200

    style changes

--- front/src/models/Footer.js
+++ front/src/models/Footer.js
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer text-center">
-      <div>
         <p className="footer-content">acamaras | Hive 2022</p>
-      </div>
     </div>
   );
 };
